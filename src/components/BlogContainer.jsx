89a53cef84ce77eb555189553b607a3706707227
ClintonCode20 <clintonmatics@gmail.com>
2024-05-19 10:36:47 +0100

    initial commit

---
+++ src/components/BlogContainer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import BlogCard from "./BlogCard";
+
+const BlogContainer = () => {
+  return (
+    <div className="container mx-auto mt-8 mb-8 px-4 flex flex-wrap justify-evenly">
+        <BlogCard />
+        <BlogCard />
+        <BlogCard />
+    </div>
+  );
+};
+
+export default BlogContainer;
